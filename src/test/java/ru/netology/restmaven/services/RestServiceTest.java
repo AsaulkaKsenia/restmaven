b0df0857a5769e6bfa5c69429c22db17f8f05108
Asaulka Ksenia <asa.ru.80@mail.ru>
2023-06-04 23:14:00 +0300

    csv file

--- src/test/java/ru/netology/restmaven/services/RestServiceTest.java
+++ src/test/java/ru/netology/restmaven/services/RestServiceTest.java
@@ -2,31 +2,20 @@ package ru.netology.restmaven.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class RestServiceTest {
 
-    @Test
-    void shouldCalculateForRest() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/rest.csv")
+
+    public void shouldCalculateForRest(int expected, int income, int expence, int treshold) {
         RestService service = new RestService();
-        int income = 10_000;
-        int expence = 3_000;
-        int treshold = 20_000;
-        int expected = 3;
 
         int actual = service.calculate(income, expence, treshold);
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    void shouldCalculateForJob() {
-        RestService service = new RestService();
-        int income = 100_000;
-        int expence = 60_000;
-        int treshold = 150_000;
-        int expected = 2;
-
-        int actual = service.calculate(income, expence, treshold);
-        Assertions.assertEquals(expected, actual);
-    }
 }
-
